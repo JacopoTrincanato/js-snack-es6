7b0a751f5966c91fee68f2ccbbfd8c838a7b5653
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-11 15:13:28 +0200

    create objects array in snack 1

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
 console.log('ciao');
+
+/*Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.*/
+
+const biciclette = [
+    {
+        name: 'bicicletta1',
+        weight: 6
+    },
+
+    {
+        name: 'bicicletta2',
+        weight: 7
+    },
+
+    {
+        name: 'bicicletta3',
+        weight: 8
+    },
+
+    {
+        name: 'bicicletta4',
+        weight: 9
+    },
+
+    {
+        name: 'bicicletta5',
+        weight: 10
+    },
+
+    {
+        name: 'bicicletta6',
+        weight: 11
+    },
+
+    {
+        name: 'bicicletta7',
+        weight: 12
+    },
+
+    {
+        name: 'bicicletta8',
+        weight: 5
+    }
+]
+
+
+
+/*Snack 2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
